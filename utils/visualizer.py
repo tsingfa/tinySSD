eec4e1a6ddfe3a5131ff32d23c9afb25d9369a1f
chenyh396 <chenyh396@mail2.sysu.edu.cn>
2022-10-31 08:00:12 +0800

    可视化

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -16,8 +16,14 @@
 
 import torch
 import matplotlib.pyplot as plt
-from test import bbox_to_rect
+import matplotlib.patches as patches
 
+def bbox_to_rect(bbox, color):
+    # 将边界框(左上x,左上y,右下x,右下y)格式转换成matplotlib格式：
+    # ((左上x,左上y),宽,高)
+    return patches.Rectangle(
+        xy=(bbox[0], bbox[1]), width=bbox[2] - bbox[0], height=bbox[3] - bbox[1],
+        fill=False, edgecolor=color, linewidth=2)
 
 def show_bboxes(axes, bboxes, labels=None, colors=None):
     """显示所有边界框"""
@@ -33,7 +39,7 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
     colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
     for i, bbox in enumerate(bboxes):
         color = colors[i % len(colors)]
-        rect = bbox_to_rect(bbox.detach().numpy(), color)
+        rect = bbox_to_rect(bbox.detach().cpu().numpy(), color)
         axes.add_patch(rect)
         if labels and len(labels) > i:
             text_color = 'k' if color == 'w' else 'w'
@@ -41,7 +47,7 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
                       va='center', ha='center', fontsize=9, color=text_color,
                       bbox=dict(facecolor=color, lw=0))
 
-def display(img, output, threshold):
+def display(img, path,output, threshold ):
     fig = plt.imshow(img)
     for row in output:
         score = float(row[1])
@@ -50,3 +56,5 @@ def display(img, output, threshold):
         h, w = img.shape[0:2]
         bbox = [row[2:6] * torch.tensor((w, h, w, h), device=row.device)]
         show_bboxes(fig.axes, bbox, '%.2f' % score, 'w')
+    plt.savefig(path)
+    plt.show()
