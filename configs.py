35c8a73b1b68fa20e4456fdd9e1a897cb8144e87
chenyh396 <chenyh396@mail2.sysu.edu.cn>
2022-10-31 08:00:40 +0800

    参数配置

---
+++ configs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Project : tinySSD
+@FileName: configs.py
+@Software: PyCharm
+
+@Time    : 2022/10/25 3:00
+@Author  : 青fa
+@Email   : tsingfa6@gmail.com
+@GitHub  : https://github.com/tsingfa
+
+功能描述:统一变量参数指定
+
+"""
+
